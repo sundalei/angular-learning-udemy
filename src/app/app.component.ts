fca1bfcef6cb4e9bd01abff957f86f1e3df52b58
Leo Sun <leosun@Leos-MacBook-Pro.local>
2022-12-09 23:00:26 +0800

    services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountsService } from './accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AccountsService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  username = "leo";
+  accounts: {name: string, status: string}[] = [];
+
+  constructor(private accountsService: AccountsService) {};
+
+  ngOnInit(): void {
+      this.accounts = this.accountsService.accounts;
+  }
+
 }
